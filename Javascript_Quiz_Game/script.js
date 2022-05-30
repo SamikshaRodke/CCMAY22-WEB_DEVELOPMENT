973534a93d3cd81baa5f04ad28bb56a2f4310815
Samiksha Rodke <104275432+SamikshaRodke@users.noreply.github.com>
2022-05-30 16:25:54 +0530

    Add files via upload

---
+++ Javascript_Quiz_Game/script.js
@@ -0,0 +1,470 @@
+const quizDB = [
+  {
+question: "Q1. Which of the following is true about cookie handling in JavaScript?",
+
+    a: "JavaScript can manipulate cookies using the cookie property of the Document object.",
+
+    b: "JavaScript can read, create, modify, and delete the cookie or cookies that apply to the current web page.",
+
+    c: "Both of the above.",
+
+    d: "None of the above",
+
+    ans: "ans3",
+},
+{
+question: "Q2. Which built-in method calls a function for each element in the array?",
+
+    a: "while()",
+
+    b: "loop()",
+
+    c: "forEach()",
+
+    d: "None of the above.",
+
+    ans: "ans3",
+},
+{
+question: "Q3. Which built-in method returns the string representation of the number's value?",
+
+    a: "toValue()",
+
+    b: "toNumber()",
+
+    c: "toString()",
+
+    d: "None of the above",
+
+    ans: "ans3",
+},
+{
+question: "Q4. Which of the following function of Boolean object returns a string of either 'true' or 'false' depending upon the value of the object?",
+
+    a: "toSource()",
+
+    b: "valueOf()",
+
+    c: "toString()",
+
+    d: "None of the above.",
+
+    ans : "ans3",
+},
+{
+question: "Q5. Which of the following function of String object returns the characters in a string beginning at the specified location through the specified number of characters?",
+
+    a: "slice()",
+
+    b: "split()",
+
+    c: "substr()",
+
+    d: "search()",
+
+    ans: "ans3:",
+},
+{
+question: "Q6. Which of the following function of String object returns the calling string value converted to upper case?",
+
+    a: "toLocaleUpperCase()",
+
+    b: "toUpperCase()",
+
+    c: "toString()",
+
+    D: "substring()",
+
+    ans: "ans2",
+},
+{
+question: "Q7. Which of the following function of String object causes a string to be displayed in the specified color as if it were in a <font color='color'> tag?",
+
+    a: "fixed()",
+
+    b: "fontcolor()",
+
+    c: "blink()",
+
+    d: "bold()",
+
+    ans: "ans2",
+},
+{
+question: "Q8. Which of the following function of Array object calls a function for each element in the array?",
+
+a: "concat()",
+
+b: "every()",
+
+c: "filter()",
+
+d: "forEach()",
+
+    ans: "ans4",
+},
+{
+question: "Q9. Which of the following function of Array object removes the first element from an array and returns that element?",
+
+    a: "reverse()",
+
+    b: "shift()",
+
+    c: "slice()",
+
+    d: "some()",
+
+    ans: "ans2",
+},
+{
+question: "Q10. JavaScript is a ___ -side programming language.",
+
+    a: "Client",
+    b: "Server",
+    c: "Both",
+    d: "None",
+
+    ans: "ans3",
+},
+{
+ question: "Q11. How do you find the minimum of x and y using JavaScript?",
+
+    a: "min(x,y);",
+    b: "Math.min(x,y)",
+    c: "Math.min(xy)",
+    d: "min(xy);",
+
+    ans: "ans2",
+},
+{
+ question: "Q12. Which JavaScript label catches all the values, except for the ones specified?",
+
+    a: "catch",
+    b: "label",
+    c: "try",
+    d: "default",
+
+    ans: "ans3",
+},
+{
+ question: "Q13. Which are the correct “if” statements to execute certain code if “x” is equal to 2?",
+
+    a: "if(x 2)",
+    b: "if(x = 2)",
+    c: "if(x == 2)",
+    d: "if(x != 2 )",
+
+    ans: "ans3",
+},
+{
+ question: "Q14. NaN stands for_________.",
+
+    a: "Nano ",                    
+
+    b: "NanTech ",  
+
+    c: "Not  a number ",     
+
+    d: "New attactedNematic",
+
+    ans: "ans3",
+},
+ {
+question: "Q15. ++ operator in Java Script is ____ Operator.",
+
+    a: "Unary ",        
+
+    b: "Positive",     
+
+    c: "Increment",  
+
+    d: "Addition",
+
+    ans: "ans1",
+
+ },
+ {
+
+question: "Q16. A translator is described as ________ software.",
+
+    a: "Application",           
+
+    b: "System ",     
+
+    c: "Utility",          
+
+    d: "Open Source",
+
+    ans: "ans2",
+
+ },
+
+ {
+
+question: "Q17. _______class provide an interface for invoking java script methods and examining java script properties.",
+
+    a: "JSobject ",  
+
+    b: "java script file",       
+
+    c: "jquary",         
+
+    d: "java wrapper",
+
+    ans: "ans1",
+
+ },
+{
+question: "Q18. When a user views a page containing a java script program, which machine actually executes the script?",
+
+    a: "The user’s machine running offline",                
+
+    b: "The user’s machine running  a web browser",           
+
+    c: "The user’s machine running on a website",    
+
+    d: "The users cookies used by a website",
+
+    ans: "ans2",
+},
+ {
+
+question: "Q19. Navigator Java script is also called _______side java script.",
+
+    a: "client",                      
+
+    b: "Server",        
+
+    c: "Website",     
+
+    d: "Live wire",
+
+    ans: "ans1",
+ },
+ 
+{
+question: "Q20. Live wire java script is also called _________side Java Script",
+
+    a: "client",                      
+
+    b: "Server",        
+
+    c: "Website",     
+
+    d: "Live wire",
+
+    ans: "ans2",
+
+},
+{
+question: "Q21. Storing numbers, dates or other values are ______ used for in java script program.",
+
+    a: "String",         
+
+    b: "Codes",        
+
+    c: "Variables",               
+
+    d: "Functions",
+
+    ans:" ans3",
+},
+ 
+{
+question: "Q22. ______ side java script statements embedded in  a HTML Page can respond to user events such as mouse-clicks, forms input and page navigation.",
+
+    a: "client",                      
+
+    b: "Server",        
+
+    c: "Website",     
+
+    d: "Live wire",
+
+    ans: "ans1",
+},
+ 
+{
+question: "Q23. When a java script object is sent to java, the runtime engine creates a java wrapper of type_______.",
+
+    a: "JSobject",    
+
+    b: "java script file",       
+
+    c: "jquary",         
+
+    d: "java wrapper",
+
+    ans: "ans1",
+},
+ 
+{
+question: "Q24. _________attribute can hold the java script version.",
+
+    a: "var",              
+
+    b: "language",              
+
+    c: "type",                        
+
+    d: "script",
+
+    ans: "ans2",
+},
+ 
+{
+question: "Q25. ________side image maps can be used with java script.",
+
+    a: "client",                      
+
+    b: "Server",        
+
+    c: "Website",     
+
+    d: "Live wire",
+
+    ans: "ans1",
+},
+{
+
+question: "Q26. In java script, A _____ is an object of the target language data type that enclose an object of the source language.",
+
+    a: "variable",     
+
+    b: "string",
+    c: "wrapper",     
+
+    d: "client",
+
+    ans: "ans3",
+},
+ 
+{
+question: "Q27. Java script is an ________scripting language.",
+
+    a: "object oriented"  ,   
+
+    b: "client oriented"  ,    
+
+    c: "network oriented",  
+
+    d: "server oriented",
+
+    ans: "ans1",
+},
+ 
+{
+question: "Q28. File is ________side java script object.",
+
+    a: "client",                      
+
+    b: "Server",        
+
+    c: "Website",     
+
+    d: "Live wire",
+
+    ans: "ans2",
+},
+
+{
+question: "Q29. File Upload is the _____ side java script object.",
+
+    a: "client",                      
+
+    b: "Server",        
+
+    c: "Website",     
+
+    d: "Live wire",
+
+    ans: "ans1",
+
+},
+{
+question: "Q30. Java script is interpreted by ______.",
+
+    a: "client",                      
+
+    b: "Server",        
+
+    c: "Website",     
+
+    d: "Live wire",
+
+    ans: "ans1",
+},
+
+];
+
+const question = document.querySelector('.question');
+const option1 = document.querySelector('#option1');
+const option2 = document.querySelector('#option2');
+const option3 = document.querySelector('#option3');
+const option4 = document.querySelector('#option4');
+const submit = document.querySelector('#submit');
+
+
+
+const answers = document.querySelectorAll('.answer');
+
+const showScore = document.querySelector('#showScore');
+
+
+
+let questionCount = 0;
+let score = 0;
+
+
+const loadQuestion =() => {
+
+    const questionList = quizDB[questionCount];
+
+    question.innerText = quizDB[questionCount].question;
+
+    option1.innerText = questionList.a;
+    option2.innerText = questionList.b;
+    option3.innerText = questionList.c;
+    option4.innerText = questionList.d;
+}
+loadQuestion();
+
+const getCheckAnswer = () => {
+    let answer;
+
+    answers.forEach((curAnsElem) => {
+        if(curAnsElem.checked){
+            answer = curAnsElem.id;
+        }
+        
+    });
+    return answer;
+};
+
+const deselectAll = () => {
+    answers.forEach((curAnsElem) => curAnsElem.checked = false);
+}
+submit.addEventListener('click', () => {
+    const checkedAnswer = getCheckAnswer();
+    console.log(checkedAnswer);
+
+    if(checkedAnswer === quizDB[questionCount].ans){
+        score++;
+    };
+
+    questionCount++;
+
+    deselectAll();
+    if(questionCount < quizDB.length){
+        loadQuestion();
+    }else{
+        showScore.innerHTML = `
+        <h3> You scored ${score}/${quizDB.length} ✌️ </h3>
+        <button class = "btn" onclick="location.reload()">Play Again</button>
+        
+        `;
+
+        showScore.classList.remove('scoreArea');
+    }
+});
+
